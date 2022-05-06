687418a0fa3fbef0e5a16b4e8851cca0349b9638
RobertLemos14 <95971607+RobertLemos14@users.noreply.github.com>
2022-05-06 19:26:04 -0300

    Create exercicio5.py

---
+++ exercicio5.py
@@ -0,0 +1,8 @@
+comprimento = input("Digite o comprimento da sala")
+largura = input("Digite a largura da sala")
+comprimento = int(comprimento)
+largura = int(largura)
+espaço_total = (comprimento) * (largura)
+produto = espaço_total / 2
+abs(produto)
+print(f"A area a ser limpa é {espaço_total} metros e deve ser usado {produto}L de produto")
